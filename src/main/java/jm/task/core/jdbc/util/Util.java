e3c5133a5d48a4f552901af342bdd69fa75f0656
AsyaSkoraya <153989033+AsyaSkoraya@users.noreply.github.com>
2024-03-03 14:53:53 +0500

    vetka1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,46 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+
+
+
     // реализуйте настройку соеденения с БД
+
+    private static final String PASS_KEY = "db.password";
+    private  static final String USERNAME_KEY = "db.username";
+    private  static final String URL_KEY = "db.url";
+
+    private Util(){
+    }
+
+    static {
+        LoadDriver();// для работы в старых версиях java
+    }
+
+
+    public static Connection open (){
+        try {
+            return DriverManager.getConnection(PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASS_KEY));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void LoadDriver() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
 }
